6a4a0033fdb4e7a87fc74c40bf141f36cda90c29
Agus <davidmersey707@gmail.com>
2026-06-21 11:22:58 +0800

    Update GenerateStatics.sh

--- WAProto/GenerateStatics.sh
+++ WAProto/GenerateStatics.sh
@@ -1,3 +1,4 @@
+cd WAProto
 sed -i 's/required /optional /g' WAProto.proto
 yarn pbjs -t static-module --no-beautify -w es6 --no-bundle --no-delimited --no-verify --no-comments -o ./index.js ./WAProto.proto;
 yarn pbjs -t static-module --no-beautify -w es6 --no-bundle --no-delimited --no-verify ./WAProto.proto | yarn pbts --no-comments -o ./index.d.ts -;
